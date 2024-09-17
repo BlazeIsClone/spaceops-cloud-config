efd142d753eaa1d0528df1692781f11124e6b9d9
BlazeIsClone <sandevabeykoon123@gmail.com>
2024-09-17 18:27:11 +0530

    docs/add: description, vpc create address

--- main.tf
+++ main.tf
@@ -8,9 +8,20 @@ terraform {
 }
 
 provider "google" {
-  project = "<PROJECT_ID>"
+  project = "spaceops"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_compute_global_address" "default" {
+  name         = "spaceops-mission-ctrl-ipv4"
+  address_type = "EXTERNAL"
+  ip_version   = "IPV4"
 }
+
+# [START gke_standard_regional_single_zone]
+resource "google_container_cluster" "default" {
+  name               = "gke-standard-regional-single-zone"
+  location           = "us-west1"
+  node_locations     = ["us-west1-c"]
+  initial_node_count = 2
+}
+# [END gke_standard_regional_single_zone]
